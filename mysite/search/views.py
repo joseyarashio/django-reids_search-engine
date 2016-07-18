cc108bc4bd7681b07182e592436a3689cbd43b99
joseyarashio <joseyarashio@gmail.com>
2016-07-18 10:09:25 +0000

    daily commit

--- mysite/search/views.py
+++ mysite/search/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import *
 from django.template import RequestContext
+from django.core.context_processors import csrf
 from django.conf import settings
 from django.core.cache import cache
 from django.views.generic import View
@@ -11,9 +12,10 @@ import os
 
 # Create your views here.
 #read cache user id
-def read_from_cache(user_name):
-    key = user_name
+def read_from_cache(key):
+
     value = cache.keys(key)
+
     if value == None:
         data = None
     else:
@@ -24,26 +26,34 @@ def read_from_cache(user_name):
 #write cache user id
 def write_to_cache(obj):
     key = obj["key"]
-    cache.set(key, json.dumps(obj), settings.NEVER_REDIS_TIMEOUT)
+
+    cache.set(str(key), json.dumps(obj), settings.NEVER_REDIS_TIMEOUT)
 
 def search(request):
     try:
+        startT = datetime.datetime.now()
         if request.method == 'GET':
 
-            # read from redis cache
-            access_hist = read_from_cache("")
+            response = {
+                'type'  :   'GET',
+                'date'  :   str(datetime.date.today().strftime("%B %d, %Y")),
+            }
 
-            response = render(request, 'search.html', {
-                # 'cookie': str(cookie),
-                'date'  : str(datetime.date.today().strftime("%B %d, %Y")),
-                'access_hist'  : access_hist,
-            })
+        if request.method == 'POST':
+            text = request.POST["tags"]
+            data = read_from_cache(':'+text+'*')
+            response = {
+                'type'  :   'POST',
+                'date'  :   str(datetime.date.today().strftime("%B %d, %Y")),
+                'data'  :   data,
+            }
 
-            # write to  redis cache
-            # write_to_cache(str(ip))
-
-            # get_redis_connection("default")
-        return response
+        endT = datetime.datetime.now()
+        pTime = endT - startT
+        response.update({'time' :   pTime.microseconds})
+        # Improttant
+        response.update(csrf(request))
+        return render(request, 'search.html', response)
 
     except TypeError:
         raise Http404()
@@ -51,21 +61,28 @@ def search(request):
     except ValueError:
         raise Http404()
 def writeall(request):
+    startT = datetime.datetime.now()
     os.chdir(r'/home/johnny/djangogirls/mysite/search')
     with open("dict.txt", "r") as f:
         lines = f.readlines()
         # read from redis cache
-        access_hist = read_from_cache("")
-        obj = {
-            'key'   :   lines[12333].replace('\n', ''),
-            'ins_date'  : str(datetime.date.today().strftime("%B %d, %Y")),
-        }
-        write_to_cache(obj)
+        for item in lines:
+            obj = {
+                'key'   :   item.replace('\n', ''),
+                'ins_date'  : str(datetime.date.today().strftime("%B %d, %Y")),
+            }
+            write_to_cache(obj)
+            # cache.set("fsdf", "fasdff", None)
+
+        endT = datetime.datetime.now()
+        pTime = endT - startT
         response = render(request, 'search.html', {
             # 'cookie': str(cookie),
-            'date'  : str(datetime.date.today().strftime("%B %d, %Y")),
-            'dict'  : lines[1234],
+            'date'  :   str(datetime.date.today().strftime("%B %d, %Y")),
+            'dict'  :   obj["key"],
+            'time'  :   pTime.microseconds
         })
+
 
         # write to  redis cache
         # write_to_cache(str(ip))
